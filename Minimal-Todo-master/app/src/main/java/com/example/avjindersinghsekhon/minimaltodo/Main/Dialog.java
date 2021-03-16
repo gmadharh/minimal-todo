6b1f1d87fdafeeeca299aa82ebdbed15aa90c06a
Nareshri Babu <nbabu@uoguelph.ca>
2021-03-16 02:34:18 -0400

    This commit closes issue #68 and refs issue/user story #18, allows the user to input text in the popup

--- Minimal-Todo-master/app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Main/Dialog.java
+++ Minimal-Todo-master/app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Main/Dialog.java
@@ -4,20 +4,30 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.widget.EditText;
 
 public class Dialog extends AppCompatDialogFragment {
 
     @Override
     public android.app.Dialog onCreateDialog(Bundle savedInstanceState) {
+
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("New Category")
-                .setMessage("Enter Category Name:")
-                .setPositiveButton ("ok", new DialogInterface.OnClickListener() {
+                .setPositiveButton ("Create Category", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
                     }
                 });
+
+        EditText editTextBox = new EditText(getActivity());
+
+        editTextBox.setHint("Enter Category Title");
+
+
+        builder.setView(editTextBox);
+        
 
         return builder.create();
     }
